e2c9e693ad2cf043cdd9f3c2e209e17d2497484b
leopiccionia <leopiccionia@gmail.com>
2025-06-05 10:29:13 -0300

    Fix tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
 
@@ -8,17 +8,21 @@ import '@testing-library/jest-dom'
 
 describe('phoneCallButton', () => {
   it('renders', async () => {
-    render(
+    const { container, getByText } = render(
       <PhoneCallButton
         targetPhoneNumbers={['+55 22 00000-0000', '+55 33 00000-0000']}
         userPhoneNumber="+55 11 00000-0000"
       />,
     )
 
-    const buttonElement = screen.getByText('Ligar')
-    expect(buttonElement).toBeInTheDocument()
+    const actionWrapper = container.firstChild
+    expect(actionWrapper).toHaveClass('bonde-phone-call bonde-phone-call--idle')
 
-    await userEvent.click(buttonElement)
-    expect(buttonElement).toHaveTextContent('Ligando')
+    const actionButton = getByText('Ligar')
+    expect(actionButton).toBeInTheDocument()
+    expect(actionButton).toHaveClass('idle')
+
+    await userEvent.click(actionButton)
+    expect(actionWrapper).toHaveClass('bonde-phone-call--ringing')
   })
 })
