7dfb06ec1525b3837fe8c7600ce2c33c4037dcf0
leopiccionia <leopiccionia@gmail.com>
2025-06-05 17:00:20 -0300

    Improve animations

--- src/PhoneCallButton/icons/Failed.tsx
+++ src/PhoneCallButton/icons/Failed.tsx
@@ -2,12 +2,12 @@ export function FailedIcon(): JSX.Element {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
       <path fill="currentColor" d="M8 3c0.5 0 2.5 4.5 2.5 5c0 1 -1.5 2 -2 3c-0.5 1 0.5 2 1.5 3c0.39 0.39 2 2 3 1.5c1 -0.5 2 -2 3 -2c0.5 0 5 2 5 2.5c0 2 -1.5 3.5 -3 4c-1.5 0.5 -2.5 0.5 -4.5 0c-2 -0.5 -3.5 -1 -6 -3.5c-2.5 -2.5 -3 -4 -3.5 -6c-0.5 -2 -0.5 -3 0 -4.5c0.5 -1.5 2 -3 4 -3Z" />
-      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} strokeDasharray={8} strokeDashoffset={8} d="M16 4l4 4">
-        <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.7s" dur="0.2s" values="8;0">
-        </animate>
-      </path>
       <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} strokeDasharray={8} strokeDashoffset={8} d="M20 4l-4 4">
         <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.9s" dur="0.2s" values="8;0">
+        </animate>
+      </path>
+      <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} strokeDasharray={8} strokeDashoffset={8} d="M16 4l4 4">
+        <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.7s" dur="0.2s" values="8;0">
         </animate>
       </path>
     </svg>
