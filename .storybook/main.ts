29ef27df96f8d296f014c479607b4c44b664c405
leopiccionia <leopiccionia@gmail.com>
2025-06-16 15:15:10 -0300

    Add a11y audit

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,10 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-actions'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {
